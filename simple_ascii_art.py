633104a646b1f755552bfabdfa04abc50df2441e
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-03-16 09:10:33 +0100

    default ascii palette moved to simple_ascii_art.py

--- simple_ascii_art.py
+++ simple_ascii_art.py
@@ -1,6 +1,18 @@
 import numpy as np
 
-def img2ascii_arr(palette: list[str], img_arr: np.ndarray, img_colors: int = 256) -> list[list[str]]:
+default_ascii_palette = [' ', '.', ':', ';', '~', '+', 'Y', 's', '3', 'V', '4', 'A', 'H', '#', 'N', '@']
+default_ascii_palette_v2 = [[' ', '.', ',', '_', '_', '_', '_', '_', '_', '_'],
+                            [' ', ':', ';', ';', ';', ';', ';', ';', ';', ';'],
+                            ['`', '-', '~', '~', 'v', 'x', 'x', 'y', 'a', 'a'],
+                            ["'", '!', '<', '\\', 'c', 'o', 'u', 'p', 'p', 'p'],
+                            ["'", '!', '+', '=', 'J', 'k', 'm', 'q', 'q', 'g'],
+                            ['"', '*', '?', '(', '[', 'A', 'm', 'G', '&', '&'],
+                            ['^', '*', 'T', 'f', 'S', '6', 'O', 'Q', 'Q', 'Q'],
+                            ['^', '*', '7', '7', 'P', '#', 'K', 'W', '@', '@'],
+                            ['^', '*', '7', 'M', '5', '#', 'R', '@', '@', '@'],
+                            ['^', '*', '7', 'M', '5', '#', 'N', '$', '$', '$']]
+
+def img2ascii_arr(img_arr: np.ndarray, img_colors: int = 256, palette: list[str] = default_ascii_palette) -> list[list[str]]:
     palette_interval = img_colors / len(palette)
     ascii_arr = []
     for i in range(img_arr.shape[0]):
@@ -9,7 +21,7 @@ def img2ascii_arr(palette: list[str], img_arr: np.ndarray, img_colors: int = 256
             ascii_arr[-1].append(palette[int(img_arr[i][j]//palette_interval)])
     return ascii_arr
 
-def img2ascii_arr_v2(palette: list[list[str]], img_arr: np.ndarray, img_colors: int = 256) -> list[list[str]]:
+def img2ascii_arr_v2(img_arr: np.ndarray, img_colors: int = 256, palette: list[list[str]] = default_ascii_palette_v2) -> list[list[str]]:
     palette_y_interval = img_colors / len(palette)
     palette_x_interval = img_colors / len(palette[0])
     ascii_arr = []
