9851ab14110d545108ba6c4586aec54b76e364e9
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-03-16 10:27:55 +0100

    quantization with dithering fix

--- img_processing.py
+++ img_processing.py
@@ -2,7 +2,10 @@ from PIL import Image
 import numpy as np
 
 def quantize(img: Image.Image, img_colors: int) -> Image.Image:
-    quantize_palette = np.array([[c,c,c] for c in range(0, 256, 256//img_colors)]).flatten()
+    if (img_colors == 1):
+        quantize_palette = np.array([0,0,0])
+    else:
+        quantize_palette = list(np.array([[c,c,c] for c in range(0, 256, 255//(img_colors-1))]).flatten())
     palette_img = Image.new("P", (1,1))
     palette_img.putpalette(quantize_palette)
     return img.quantize(palette=palette_img)
