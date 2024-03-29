b0f3007ac203757f1deb811f97071b1842036819
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-03-29 20:12:50 +0100

    refactoring

--- img_processing.py
+++ img_processing.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps, ImageEnhance
 import numpy as np
 
 def quantize(img: Image.Image, img_colors: int) -> Image.Image:
@@ -9,3 +9,17 @@ def quantize(img: Image.Image, img_colors: int) -> Image.Image:
     palette_img = Image.new("P", (1,1))
     palette_img.putpalette(quantize_palette)
     return img.quantize(palette=palette_img)
+
+def preprocess_img(img: Image.Image,
+                   scale_factor=1,
+                   contrast=1,
+                   brightness=1,
+                   eq=0,
+                   quantize_colors=255):
+    img = ImageOps.scale(img, scale_factor, Image.Resampling.BOX)
+    img = ImageEnhance.Contrast(img).enhance(contrast)
+    img = ImageEnhance.Brightness(img).enhance(brightness)
+    img = Image.blend(img, ImageOps.equalize(img), eq)
+    img = quantize(img, quantize_colors)
+    img = img.convert("L")
+    return img
