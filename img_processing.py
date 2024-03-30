80bac15db824d98bae5a76ad76186fe73d89025f
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-03-30 14:58:25 +0100

    error diffusion dithering methods

--- img_processing.py
+++ img_processing.py
@@ -13,21 +13,42 @@ def generate_bayer_matrix(n):
     return (1 / n_sq) * m
 
 dither_bayer_m = generate_bayer_matrix(32) - 0.5
+jjn_k = np.array([
+    [0, 0, 0, 7, 5],
+    [3, 5, 7, 5, 3],
+    [1, 3, 5, 3, 1]
+]) / 48
+
+fs_k = np.array([
+    [0, 0, 7],
+    [3, 5, 1]
+]) / 16
 
 def apply_threshold_map(c, m, r, x, y):
     n = len(m)
     return c + r * m[x % n][y % n]
 
-def quantize(img: Image.Image, img_colors: int) -> Image.Image:
-    if (img_colors == 1):
-        quantize_palette = np.array([0,0,0])
-    else:
-        quantize_palette = list(np.array([[c,c,c] for c in range(0, 256, 255//(img_colors-1))]).flatten())
-    palette_img = Image.new("P", (1,1))
-    palette_img.putpalette(quantize_palette)
-    return img.quantize(palette=palette_img)
+def apply_jjn_error_diff(c, c_new, img_arr, x, y):
+    c_err = c - c_new
+    for k_y in range(0, jjn_k.shape[0]):
+        for k_x in range(0, jjn_k.shape[1]):
+            if (y + k_y >= img_arr.shape[0]):
+                continue
+            if (x + k_x - 2 >= img_arr.shape[1] or x + k_x - 2 < 0):
+                continue
+            img_arr[y + k_y][x + k_x - 2] += c_err * jjn_k[k_y][k_x]
 
-def quantize_grayscale(img: Image.Image, img_colors: int) -> Image.Image:
+def apply_fs_error_diff(c, c_new, img_arr, x, y):
+    c_err = c - c_new
+    for k_y in range(0, fs_k.shape[0]):
+        for k_x in range(0, fs_k.shape[1]):
+            if (y + k_y >= img_arr.shape[0]):
+                continue
+            if (x + k_x - 1 >= img_arr.shape[1] or x + k_x - 1 < 0):
+                continue
+            img_arr[y + k_y][x + k_x - 1] += c_err * fs_k[k_y][k_x]
+
+def quantize_grayscale(img: Image.Image, img_colors: int, dither: str = "NONE") -> Image.Image:
     if (img.mode != "L"):
         raise "wrong img mode"
     if (img_colors <= 0):
@@ -36,12 +57,26 @@ def quantize_grayscale(img: Image.Image, img_colors: int) -> Image.Image:
     m = dither_bayer_m
     img_arr = np.array(img)
     color_step = 256 / img_colors
-    palette = np.linspace(0, 255, img_colors)
+    palette = np.linspace(0, 255, img_colors, dtype=np.int16)
+
     for y in range(0, img_arr.shape[0]):
         for x in range(0, img_arr.shape[1]):
-            c_dithered = int(apply_threshold_map(img_arr[y][x], m, color_step, x, y) / color_step)
-            c_dithered = min(len(palette) - 1, max(0, c_dithered))
-            img_arr[y][x] = palette[c_dithered]
+            c = img_arr[y][x]
+            c_new = c
+            if (dither == "BAYER"):
+                c_new = apply_threshold_map(c, m, color_step, x, y)
+
+            c_new_idx = int(c_new / color_step)
+            c_new_idx = min(len(palette) - 1, max(0, c_new_idx))
+            c_new = palette[c_new_idx]
+            
+            if (dither == "JJN"):
+                apply_jjn_error_diff(c, c_new, img_arr, x, y)
+            
+            if (dither == "FS"):
+                apply_fs_error_diff(c, c_new, img_arr, x, y)
+
+            img_arr[y][x] = c_new
     
     return Image.frombytes("L", img_arr.shape, img_arr)
 
@@ -56,6 +91,6 @@ def preprocess_img(img: Image.Image,
     img = ImageEnhance.Contrast(img).enhance(contrast)
     img = ImageEnhance.Brightness(img).enhance(brightness)
     img = Image.blend(img, ImageOps.equalize(img), eq)
-    img = quantize_grayscale(img, quantize_colors)
+    img = quantize_grayscale(img, quantize_colors, "FS")
     img = img.convert("L")
     return img
