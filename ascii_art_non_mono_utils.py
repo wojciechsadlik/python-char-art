9e701b71420aad9a9b8701dd382c8d650f5bfce2
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-05-18 21:26:42 +0200

    cleanup

--- ascii_art_non_mono_utils.py
+++ ascii_art_non_mono_utils.py
@@ -7,3 +7,20 @@ def new_img_draw(size, fill=0):
 
 def clear_img(draw, size, fill=0):
     draw.rectangle(((0,0), size), fill=fill)
+
+def split_lines(img, palette, font):
+    _, draw = new_img_draw(img.size)
+    text = []
+    bbox = draw.textbbox((0,0), ''.join(text), font=font)
+    img_size = img.size
+    lines = []
+    while bbox[3] < img_size[1]:
+        text.append(''.join(palette) + '\n')
+        bbox = draw.textbbox((0,0), ''.join(text), font=font)
+
+    line_width = img.size[0]
+    line_height = img.size[1] // len(text)
+    lines = []
+    for i in range(len(text)):
+        lines.append(img.crop((0, i * line_height, line_width, (i+1) * line_height)))
+    return lines
