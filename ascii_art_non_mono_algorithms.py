2e8d2c376a5ede032b23479a98da9027292fca64
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-05-19 13:35:53 +0200

    single line harmony search

--- ascii_art_non_mono_algorithms.py
+++ ascii_art_non_mono_algorithms.py
@@ -11,7 +11,7 @@ def generate_random_line(line, palette, font):
     bbox = text_draw.textbbox((0,0), ''.join(text_arr), font=font)
 
     while line_size[0] > bbox[2]:
-        text_arr.append(palette[random.randint(0, len(palette)-1)])
+        text_arr.append(palette[random.randrange(0, len(palette))])
         bbox = text_draw.textbbox((0,0), ''.join(text_arr), font=font)
 
     text_arr.pop()
@@ -21,7 +21,7 @@ def generate_random_line(line, palette, font):
 
 def lazy_random_search(img, palette, font):
     lines = split_lines(img, palette, font)
-    best_diff = math.inf
+    best_fit = -math.inf
     best_text_arr = []
 
     while True:
@@ -29,10 +29,10 @@ def lazy_random_search(img, palette, font):
         for l in lines:
             r_l_text = generate_random_line(l, palette, font)
             text_arr.append(''.join(r_l_text) + '\n')
-        diff = evaluate_text_arr(text_arr, img, font)
-        if diff < best_diff:
+        fit = evaluate_text_arr(text_arr, img, font)
+        if fit > best_fit:
             best_text_arr = text_arr
-            best_diff = diff
+            best_fit = fit
         yield best_text_arr
         
 
@@ -45,14 +45,14 @@ def generate_greedy_line(line, palette, font):
     while line_size[0] > bbox[2]:
         best_c = palette[0]
         text_arr.append(palette[0])
-        best_c_diff = evaluate_text_arr(text_arr, line, font)
+        best_c_fit = evaluate_text_arr(text_arr, line, font)
         text_arr.pop()
         for i in range(1, len(palette)):
             text_arr.append(palette[i])
-            diff = evaluate_text_arr(text_arr, line, font)
-            if diff < best_c_diff:
+            fit = evaluate_text_arr(text_arr, line, font)
+            if fit > best_c_fit:
                 best_c = palette[i]
-                best_c_diff = diff
+                best_c_fit = fit
             text_arr.pop()
         text_arr.append(best_c)
         bbox = text_draw.textbbox((0,0), ''.join(text_arr), font=font)
@@ -72,4 +72,41 @@ def greedy_algorithm(img, palette, font):
 
     return text_arr
 
+
+def generate_line_population(line, palette, font, count):
+    population = []
+    population.append(text_arr_to_palette_id_arr(generate_greedy_line(line, palette, font), palette))
+    for _ in range(1, count):
+        population.append(text_arr_to_palette_id_arr(generate_random_line(line, palette, font), palette))
+
+    align_population_lengths(population, len(palette))
+
+    population, fits = sort_population(population, palette, line, font)
+
+    return population, fits
+
+
+def generate_harmony_line(line, palette, font, iterations=100, pop_count=10, mem_rate=0.8, pa_rate=0.3, bw=2):
+    population, fits = generate_line_population(line, palette, font, pop_count)
+    el_len = len(population[0])
+    
+    for i in range(iterations):
+        new_harm = []
+        while len(new_harm) < el_len:
+            if (random.random() < mem_rate):
+                new_pitch = population[random.randrange(0, pop_count)][len(new_harm)]
+                if (random.random() < pa_rate):
+                    new_pitch += random.randint(-bw//2, bw//2)
+                    new_pitch = new_pitch % len(palette)
+                new_harm.append(new_pitch)
+            else:
+                new_harm.append(random.randrange(0, len(palette)))
+        
+        insert_into_sorted_population(population, fits, new_harm, palette, line, font)
+
+        if i % 1000 == 0:
+            print(min(fits), np.mean(fits))
+
+    return population, fits
+            
 
--- ascii_art_non_mono_utils.py
+++ ascii_art_non_mono_utils.py
@@ -1,3 +1,4 @@
+import random
 from PIL import Image, ImageDraw, ImageChops
 import numpy as np
 
@@ -26,13 +27,56 @@ def split_lines(img, palette, font):
         lines.append(img.crop((0, i * line_height, line_width, (i+1) * line_height)))
     return lines
 
-def palette_ids_to_text_arr(p_id_arr, palette):
+def palette_id_arr_to_text_arr(p_id_arr, palette):
     text_arr = []
     for id in p_id_arr:
         text_arr.append(palette[id])
     return text_arr
 
+def text_arr_to_palette_id_arr(text_arr, palette):
+    p_id_arr = []
+    for c in text_arr:
+        p_id_arr.append(palette.index(c))
+    return p_id_arr
+
 def evaluate_text_arr(text_arr, img, font):
     text_img, text_draw = new_img_draw(img.size, int(np.mean(img)))
     text_draw.text((0,0), ''.join(text_arr), font=font, fill=255)
-    return np.mean(ImageChops.difference(text_img, img))
+    return -np.mean(ImageChops.difference(text_img, img))
+
+def evaluate_palette_id_arr(p_id_arr, palette, img, font):
+    text_arr = palette_id_arr_to_text_arr(p_id_arr, palette)
+    return evaluate_text_arr(text_arr, img, font)
+
+def evaluate_palette_id_population(population, palette, img, font):
+    pop_fit = []
+    for el in population:
+        pop_fit.append(evaluate_palette_id_arr(el, palette, img, font))
+    return pop_fit
+
+def align_population_lengths(population, palette_len):
+    max_len = len(population[0])
+    for i in range(1, len(population)):
+        if (len(population[i])) > max_len:
+            max_len = len(population[i])
+
+    for i in range(len(population)):
+        while len(population[i]) < max_len:
+            population[i].append(random.randrange(0, palette_len))
+
+def sort_population(population, palette, img, font):
+    fits = evaluate_palette_id_population(population, palette, img, font)
+    sorted_population = sorted(zip(fits, population), reverse=True)
+    sorted_fits = list(map(lambda x: x[0], sorted_population))
+    sorted_population = list(map(lambda x: x[1], sorted_population))
+    return sorted_population, sorted_fits
+
+def insert_into_sorted_population(population, fits, new_el, palette, img, font):
+    new_fit = evaluate_palette_id_arr(new_el, palette, img, font)
+    for i, f in enumerate(fits):
+        if new_fit > f:
+            fits.insert(i, new_fit)
+            fits.pop()
+            population.insert(i, new_el)
+            population.pop()
+            return
