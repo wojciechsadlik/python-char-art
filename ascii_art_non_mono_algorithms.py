2a1ff225ef663d68e34a7bf62565b6657900d7f5
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2024-06-01 11:19:25 +0200

    harmony workers fix

--- ascii_art_non_mono_algorithms.py
+++ ascii_art_non_mono_algorithms.py
@@ -91,27 +91,28 @@ def generate_line_population(line, palette, font, count, include_greedy=False):
 
     return population, fits
 
+def new_harmony_line(palette, population, mem_rate=0.8, pa_rate=0.3, bw=2):
+    new_harm = []
+    while len(new_harm) < len(population[0]):
+        if (random.random() < mem_rate):
+            new_pitch = population[random.randrange(0, len(population))][len(new_harm)]
+            if (random.random() < pa_rate):
+                new_pitch += random.randint(-bw, bw) // 2
+                new_pitch = new_pitch % len(palette)
+            new_harm.append(new_pitch)
+        else:
+            new_harm.append(random.randrange(0, len(palette)))
+    
+    return new_harm
 
 def lazy_generate_harmony_line(line, palette, font, pop_count=10, mem_rate=0.8, pa_rate=0.3, bw=2, include_greedy=False):
     population, fits = generate_line_population(line, palette, font, pop_count, include_greedy)
-    el_len = len(population[0])
     generation = 0
     
     while True:
         generation += 1
-        new_harm = []
-        while len(new_harm) < el_len:
-            if (random.random() < mem_rate):
-                new_pitch = population[random.randrange(0, pop_count)][len(new_harm)]
-                if (random.random() < pa_rate):
-                    new_pitch += random.randint(-bw, bw) // 2
-                    new_pitch = new_pitch % len(palette)
-                new_harm.append(new_pitch)
-            else:
-                new_harm.append(random.randrange(0, len(palette)))
-        
+        new_harm = new_harmony_line(palette, population, mem_rate, pa_rate, bw)
         insert_into_sorted_population(population, fits, new_harm, palette, line, font)
-        
         yield population[0], fits[0]
 
 class HarmonyLineSearch:
@@ -119,31 +120,21 @@ class HarmonyLineSearch:
         self.line = line
         self.palette = palette
         self.font = font
-        self.pop_count = pop_count
         self.mem_rate = mem_rate
         self.pa_rate = pa_rate
         self.bw = bw
         self.population, self.fits = generate_line_population(line, palette, font, pop_count, include_greedy)
         self.generation = 0
-        self.el_len = len(self.population[0])
 
     def next_line(self, incr):
         self.generation += incr
         for _ in range(incr):
-            new_harm = []
-            while len(new_harm) < self.el_len:
-                if (random.random() < self.mem_rate):
-                    new_pitch = self.population[random.randrange(0, self.pop_count)][len(new_harm)]
-                    if (random.random() < self.pa_rate):
-                        new_pitch += random.randint(-self.bw, self.bw) // 2
-                        new_pitch = new_pitch % len(self.palette)
-                    new_harm.append(new_pitch)
-                else:
-                    new_harm.append(random.randrange(0, len(self.palette)))
-            
+            new_harm = new_harmony_line(self.palette, self.population, self.mem_rate, self.pa_rate, self.bw)
             insert_into_sorted_population(self.population, self.fits, new_harm, self.palette, self.line, self.font)
-        
-        return self.population[0], self.fits[0]
+        return vars(self)
+    
+    def get_solution(self):
+        return self.population[0]
     
 def lazy_harmony_search(img, palette, font, pop_count=10, mem_rate=0.8, pa_rate=0.3, bw=2, include_greedy=False):
     lines = split_lines(img, palette, font)
@@ -165,10 +156,11 @@ def pool_harmony_search(img, palette, font, generations, pop_count=10, mem_rate=
         line_finders.append(HarmonyLineSearch(l, palette, font, pop_count, mem_rate, pa_rate, bw, include_greedy))
 
     while True:
-        text_arr = []
         with Pool(USE_CPU) as p:
-            l_p_ids = p.starmap(HarmonyLineSearch.next_line, zip(line_finders, repeat(generations)))
-        l_p_ids = list(l[0] for l in l_p_ids)
+            next_states = p.starmap(HarmonyLineSearch.next_line, zip(line_finders, repeat(generations)))
+        for f, s in zip(line_finders, next_states):
+            f.__dict__.update(s)
+        l_p_ids = map(HarmonyLineSearch.get_solution, line_finders)
         l_text_arr = starmap(palette_id_arr_to_text_arr, zip(l_p_ids, repeat(palette)))
         text_arr = list(l + '\n' for l in map(''.join, l_text_arr))
         yield text_arr
