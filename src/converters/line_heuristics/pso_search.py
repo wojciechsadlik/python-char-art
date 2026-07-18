3d17b5a480abeca77fb46d106ecc10cc7e6ca4e8
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2026-07-18 11:17:38 +0200

    pso->dpso

--- src/converters/line_heuristics/pso_search.py
+++ src/converters/line_heuristics/pso_search.py
@@ -27,52 +27,49 @@ class ParticleSwarmLineSearch(LineConverter):
     def line2symbols(self, line: Image.Image) -> list[str]:
         particles, fits = generate_line_population(
             line, self.symbols, self.font, self.pop_count, self.include_greedy)
-        particles_np = np.array(particles, dtype=np.float32)
+        particles_np = np.array(particles, dtype=np.int32)
         particles_stag_counter = [0] * self.pop_count
         pos_len = len(particles_np[0])
+        num_symbols = len(self.symbols)
 
         best_particle_pos = copy.deepcopy(particles_np)
         best_particle_pos_fit = copy.deepcopy(fits)
         best_global_pos = copy.deepcopy(particles_np[0])
         best_global_pos_fit = fits[0]
-        # print(-1, best_global_pos_fit)
 
-        vmin = 0.5
-        vmax = 0.4 * len(self.symbols)
-
-        velocities = [(np.random.random(pos_len) - 0.5) * vmax
-                      for _ in range(len(particles_np))]
-
+        velocities = np.zeros((self.pop_count, pos_len, num_symbols), dtype=np.float32)
 
         for gen in range(self.generations):
             innertion = self.init_innertion * (self.generations - gen) / self.generations
-            for i in range(len(particles_np)):
+            for i in range(self.pop_count):
                 if particles_stag_counter[i] > 30:
-                    particles_np[i] = np.random.random(particles_np[i].shape) \
-                                      * len(self.symbols)
+                    particles_np[i] = np.random.randint(0, num_symbols, size=pos_len)
                     particles_stag_counter[i] = 0
 
-                r_p = np.random.random(velocities[i].shape)
-                r_g = np.random.random(velocities[i].shape)
+                r_p = np.random.random((pos_len, 1))
+                r_g = np.random.random((pos_len, 1))
+
+                pbest_onehot = np.zeros((pos_len, num_symbols), dtype=np.float32)
+                pbest_onehot[np.arange(pos_len), best_particle_pos[i]] = 1.0
+
+                gbest_onehot = np.zeros((pos_len, num_symbols), dtype=np.float32)
+                gbest_onehot[np.arange(pos_len), best_global_pos] = 1.0
 
                 v = innertion * velocities[i]
-                v += self.cog_coeff * r_p * \
-                    (best_particle_pos[i] - particles_np[i])
-                v += self.soc_coeff * r_g * (best_global_pos - particles_np[i])
-
-                if np.linalg.norm(v) < vmin:
-                    particles_np[i] = (np.random.random(pos_len) - 0.5) * vmax
-                    
-                # if np.linalg.norm(v) > vmax:
-                #     v *= vmax / np.linalg.norm(v)
-
+                v += self.cog_coeff * r_p * pbest_onehot
+                v += self.soc_coeff * r_g * gbest_onehot
                 velocities[i] = v
-                particles_np[i] += velocities[i]
-                # particles_np[i] = particles_np[i] % len(self.symbols)
+
+                v_shifted = v - np.max(v, axis=1, keepdims=True)
+                exp_v = np.exp(v_shifted)
+                probs = exp_v / np.sum(exp_v, axis=1, keepdims=True)
+
+                for j in range(pos_len):
+                    particles_np[i, j] = np.random.choice(num_symbols, p=probs[j])
 
                 try:
                     fits[i] = evaluate_symbols_id_arr(
-                        list(map(int, particles_np[i])), self.symbols, line, self.font)
+                        particles_np[i].tolist(), self.symbols, line, self.font)
                 except IndexError:
                     fits[i] = -float('inf')
 
@@ -87,9 +84,5 @@ class ParticleSwarmLineSearch(LineConverter):
                 else:
                     particles_stag_counter[i] += 1
 
-            # print(gen)
-            # print('avg velocity', np.mean(np.linalg.norm(velocities, axis=1)))
-            # print('avg fit', np.mean(best_particle_pos_fit))
-
-        best_p_id_arr = list(map(int, best_global_pos))
+        best_p_id_arr = best_global_pos.tolist()
         return symbols_id_arr_to_text_arr(best_p_id_arr, self.symbols)
